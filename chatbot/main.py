7e3aa9981110dcbe4d5264eafc0f40809dc01048
Bakurecz Zoltán <z.bakurecz@gmail.com>
2025-02-04 12:52:45 +0100

    SGR-A-STELLA: kódtisztítás

--- chatbot/main.py
+++ chatbot/main.py
@@ -3,12 +3,3 @@ from routers.messages import router as messages_router
 
 app = FastAPI()
 app.include_router(messages_router)
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
