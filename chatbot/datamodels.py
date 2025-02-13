f773660ed066bcd7eb35cff542c125da663c98f9
Bakurecz Zoltán <z.bakurecz@gmail.com>
2025-02-13 17:40:19 +0100

    foreign key comments and missing uniqe constraints, delete tests log file

--- chatbot/datamodels.py
+++ chatbot/datamodels.py
@@ -12,6 +12,9 @@ from sqlmodel import Field, SQLModel
 class Message(SQLModel, table=True):
     __tablename__ = 'chatbot_message'
     id: typing.Union[int, None] = Field(default=None, primary_key=True)
+    # FIXME: logic (endpoint /register, login,... etc./ and CRUD/persistence) refactor for using foreign key
+    #user_id: typing.Union[int, None] = Field(default=None, foreign_key='chatbot_user.id')
+    # FIXME: business id reference to table/model chatbot_user/User without foreign key (present logic requires this)
     username: str = Field(index=True)
     role: str = Field(index=True)
     message_text: str
@@ -21,9 +24,9 @@ class Message(SQLModel, table=True):
 class User(SQLModel, table=True):
     __tablename__ = 'chatbot_user'
     id: typing.Union[int, None] = Field(default=None, primary_key=True)
-    username: str = Field(index=True)
+    username: str = Field(index=True, unique=True)
     full_name: str = Field(index=True)
-    email: str = Field(index=True)
+    email: str = Field(index=True, unique=True)
     hashed_pw: str
     disabled: bool
     last_login: datetime
